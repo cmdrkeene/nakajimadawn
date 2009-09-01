e091fa388dae51cd356b4e0893761157657ed4c8
Brandon Keene <bkeene@gmail.com>
2009-08-31 21:05:30 -0400

    Replaced error handling

--- app/controllers/sync_controller.rb
+++ app/controllers/sync_controller.rb
@@ -1,15 +1,14 @@
 class SyncController < ApplicationController
   def show
-    # begin
+    begin
       response = Nakajima.sync!
-      raise response["error"] if response["error"]
       if response.size > 0
         render :text => "#{response.size} new tweet#{'s' if response.size > 1} from @nakajima", :status => 201
       else
         render :text => nil, :response => 304
       end
-    # rescue Exception => exception
-    #   render :text => "Error: #{exception}"
-    # end
+    rescue Exception => exception
+      render :text => "Error: #{exception}"
+    end
   end
 end
