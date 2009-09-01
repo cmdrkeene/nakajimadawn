8a6a027dfca961ada3291e4c1e3c7d1d48fa5e16
Brandon Keene <bkeene@gmail.com>
2009-08-31 20:17:09 -0400

    Added explicit handling of httparty error

--- app/controllers/sync_controller.rb
+++ app/controllers/sync_controller.rb
@@ -2,6 +2,7 @@ class SyncController < ApplicationController
   def show
     begin
       response = Nakajima.sync!
+      raise response["error"] if response["error"]
       if response.size > 0
         render :text => "#{response.size} new tweet#{'s' if response.size > 1} from @nakajima", :status => 201
       else
