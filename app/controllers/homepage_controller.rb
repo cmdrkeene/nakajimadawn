09be286f0a23d7dc169eeed0ad2a4c70e2dc0387
Brandon Keene <bkeene@gmail.com>
2009-08-31 21:31:22 -0400

    Fixed template error; Added cache-control of 1 hour to home page

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,5 +1,7 @@
 class HomepageController < ApplicationController
   def index
+    expires_in 1.hour, :public => true
+    
     @tweets = Tweet.paginate :order     => 'tweeted_at DESC',
                              :page      => params[:page],
                              :per_page  => 10
