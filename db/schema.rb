61ff37c6a1aa24d78d3f0cb1ce414df9facdba44
Brandon Keene <bkeene@gmail.com>
2009-10-06 09:16:10 -0400

    Adding redacted and hidden flags to tweets; Display only visible tweets by default

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20090830181211) do
+ActiveRecord::Schema.define(:version => 20091006131135) do
 
   create_table "tweets", :force => true do |t|
     t.text     "text"
@@ -22,6 +22,8 @@ ActiveRecord::Schema.define(:version => 20090830181211) do
     t.datetime "tweeted_at"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "redacted",                :default => false
+    t.boolean  "hidden",                  :default => false
   end
 
 end
--- spec/controllers/homepage_controller_spec.rb
+++ spec/controllers/homepage_controller_spec.rb
@@ -2,22 +2,28 @@ require File.dirname(__FILE__) + '/../spec_helper'
 
 describe HomepageController do
   integrate_views
-  
+
   describe "#index" do
     before do
       11.times { Factory(:nakajima_dawn_tweet) }
     end
-    
+
     it "should show 10 tweets per page" do
       get :index
       assigns[:tweets].size.should == 10
     end
-    
+
     it "should paginate tweets" do
       get :index, :page => 2
       assigns[:tweets].size.should == 1
     end
-    
+
+    it "should not show hidden tweets" do
+      hidden_tweet = Factory(:nakajima_dawn_tweet, :hidden => true)
+      get :index, :page => 2
+      assigns[:tweets].should_not include(hidden_tweet)
+    end
+
     it "should have an RSS feed"
     it "should have an iPhone view"
   end
